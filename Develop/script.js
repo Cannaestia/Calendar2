ee45090967a5fa8db8888a7a08387be1f1c0e135
Jennifer Arnold <canna@macbook-pro.lan>
2022-09-19 17:55:12 -0400

    added colors

---
+++ Develop/script.js
@@ -0,0 +1,29 @@
+$('.container').on('click', '.save', function(){
+    var parentId = $(this).parent().attr('data-id');
+    var appt = $(this).prev().val();
+    console.log(appt);
+    localStorage.setItem(parentId, appt);
+  });
+
+  $('.appt').each(function(){
+    var parentId = $(this).parent().attr('data-id');
+    var appt = localStorage.getItem(parentId);
+    $(this).val(appt);
+  })
+
+  var date = moment().format('dddd MMM Do YYYY');
+      $('#currentDay').text(date);
+
+$('textarea').each(function(){
+    var currentTime = moment().format('HH');
+    var dataID = $(this).attr('data-hour');
+    if (currentTime < dataID) {
+       $(this).attr("class", "past");
+    } else if (currentTime === dataID) {
+        $(this).attr("class","present");
+    } else if (currentTime > dataID) {
+        $(this).attr("class","future");
+    }
+    console.log(currentTime);
+
+})
